79be18f785378f9dc3030aba2389565147a45952
grzegorzTomasik <grzegorz.tomasik@yumasoft.pl>
2020-01-05 00:46:54 +0100

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ const hbs = require('hbs')
 
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+
 const app = express()
+
+const port = process.env.PORT || 3000
+
 const publicDir = path.join(__dirname, '../public' )
 
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -84,6 +88,6 @@ app.get('*', (req, res) => {
         errorMessage: 'Page not found'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
