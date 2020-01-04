0c484dd5f971e2cd7da85a019bb2320fb47427b2
grzegorzTomasik <grzegorz.tomasik@yumasoft.pl>
2020-01-04 23:31:49 +0100

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.getElementById('message-1') 
+const messageTwo = document.getElementById('message-2') 
+
+
+weatherForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    messageOne.textContent = 'Loading ...'
+    messageTwo.textContent = ''
+    
+    const location = search.value
+    fetch(`http://localhost:3000/weather?address=${location}`)
+        .then(response => {
+            response.json().then(data => {
+                if(data.error) {
+                    messageOne.textContent = data.error
+                } else {
+                    messageOne.textContent = data.location
+                    messageTwo.textContent = data.forecast
+                }
+                
+            })
+        })
+})
+
---
+++ src/app.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const app = express()
+const publicDir = path.join(__dirname, '../public' )
+
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDir))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Grzegorz Tomasik'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Grzegorz Tomasik'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'HELP me',
+        title: 'Help',
+        name: 'Grzegorz Tomasik'
+
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'No search conditions'
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location} = {}) => {
+        if ( error ) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+    //res.send({
+    //    forecast: 'Snowing',
+    //    location: 'Philadelphia',
+    //    address: req.query.address
+    //})
+})
+
+
+app.get('/help/*', (req, res) => {
+    res.render('404',{
+        title: '404',
+        name: 'Grzegorz Tomasik',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Grzegorz Tomasik',
+        errorMessage: 'Page not found'
+    })
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
